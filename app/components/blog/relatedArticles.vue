690f3691f3c156371638bb47cc19a60973d83458
zxml7777777 <zxml6666l@gmail.com>
2025-03-06 22:04:23 +0800

    Initial commit

---
+++ app/components/blog/relatedArticles.vue
@@ -0,0 +1,46 @@
+<script setup>
+defineProps({
+  surround: {
+    type: Array,
+    required: true
+  },
+  spacing: {
+    type: Boolean,
+    default: true,
+  },
+})
+</script>
+
+<template>
+  <div>
+    <span class="blog-aside-title mb-2 block">Continue Reading</span>
+    <ul
+      class="list-none"
+      :class="{
+        'ml-4': spacing,
+        'p-0 m-0': !spacing,
+      }"
+    >
+      <template
+        v-for="(other, index) in surround"
+        :key="index"
+      >
+        <li
+          v-if="other"
+          class="first:mt-0 mt-4 md:mt-1"
+        >
+          <NuxtLink
+            :to="`${other._path}/`"
+            class="hover:underline hover:text-brand_primary"
+            :aria-label="other.headline"
+          >
+            {{ other.headline }}
+          </NuxtLink>
+          <p class="!no-underline md:hidden text-sm leading-sm md:text-xs md:leading-xs mt-1">
+            {{ other.excerpt }}
+          </p>
+        </li>
+      </template>
+    </ul>
+  </div>
+</template>
---
+++ app/pages/app/Dashboard.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+const { t } = useI18n()
+const localePath = useLocalePath()
+useHead({ title: t('dashboard.pageTitle') })
+</script>
+
+<template>
+  <div class="dashboard container mx-auto px-4">
+    <section class="flex items-center justify-between border-b pb-4 border-neutral-300 text-neutral-400 font-xl">
+      <p>你已经踏上了成功之路！</p>
+      <p>你的项目:</p>
+    </section>
+  </div>
+</template>
+
+<style lang="scss">
+.cv-dashboard {
+  tbody tr td:first-child {
+    @apply text-brand-blue;
+    text-decoration: underline;
+
+    &:hover {
+      text-decoration: none;
+      @apply text-gray-500;
+    }
+  }
+}
+</style>
